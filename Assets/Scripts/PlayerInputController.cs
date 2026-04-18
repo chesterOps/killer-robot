bc7716f9a3ef23d5488471d6671eaee43a355d5c
Chester Okwulehie <ochester4@gmail.com>
2026-04-18 20:12:14 +0100

    Implemented player jump

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,13 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputController : MonoBehaviour
 {
     public Vector3 MovementDirectionVector { get; private set; }
+    public event Action OnJumpButtonPressed;
 
     void OnMove(InputValue inputValue)
     {
         Vector2 direction = inputValue.Get<Vector2>();
         MovementDirectionVector = new(direction.x, 0, direction.y);
+    }
+
+    void OnJump(InputValue inputvalue)
+    {
+        if (inputvalue.isPressed)
+        {
+            OnJumpButtonPressed?.Invoke();
+        }
     }
 }
